64d254209f92c29ef6777f71c7727b277234f9e7
zhangyt <huli115386945@126.com>
2020-04-07 19:07:22 +0800

    add taskresult

--- src/task.rs
+++ src/task.rs
@@ -22,17 +22,29 @@ impl<'a, T> Task<'a, T> {
     }
 }
 
-pub struct MyCmd {
-    id: u64,
+pub trait RawResult {
+    fn to_str(&self) -> &str;
+    fn to_json(&self) -> &str;
 }
 
-impl MyCmd {
-    fn new(id:u64) -> Self {
-        return MyCmd{id:id};
+pub struct TaskResult<T:RawResult> {
+    id :u64,
+    code:u8,
+    result:T,
+}
+
+impl<T:RawResult> TaskResult<T> {
+    fn new(id:u64, code:u8, result:T) -> Self {
+        return TaskResult{id:id, code:code, result:result};
     }
-    fn string(&self) -> String {
-        let s = self.id.to_string();
-        s
+    fn get_id(&self) -> &u64 {
+        &self.id
+    }
+    fn get_code(&self) -> &u8 {
+        &self.code
+    }
+    fn get_result(&self) -> &T {
+        &self.result
     }
 }
 
@@ -52,6 +64,26 @@ mod test {
             s
         }
     }
+
+    pub struct MyResult<'a> {
+        err_msg : &'a str,
+    }
+
+    impl<'a> MyResult<'a> {
+        fn new(msg:&'a str) -> Self {
+            return MyResult{err_msg:msg};
+        }
+    }
+
+    impl<'a> RawResult for MyResult<'a> {
+        fn to_str(&self) -> &str {
+            &self.err_msg 
+        }
+        fn to_json(&self) -> &str {
+            &self.err_msg 
+        }
+    }
+
     #[test]
     fn test_task() {
         let mut t:Task<MyCmd> = Task::new(12345, "hello world");
@@ -62,5 +94,14 @@ mod test {
         let movetest = t.at(0).id;
         assert_eq!(movetest, 123456);
         assert_eq!(t.at(0).id, 123456);
+
+        let mut err_msg = "我错了";
+        let myRet = MyResult::new(err_msg);
+        assert_eq!(err_msg, "我错了");
+        let ret = TaskResult::new(12345, 200, myRet);
+        assert_eq!(ret.get_result().to_str(), "我错了");
+        err_msg = "我又错了";
+        assert_eq!(err_msg, "我又错了");
+        assert_eq!(ret.get_result().to_str(), "我错了");
     }
 }
